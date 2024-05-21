192e841c6b3086fdf21f4aa5024337ab9a441e5a
Gokulachalam <gokul2003ai@gmail.com>
2024-05-21 05:43:03 +0530

    commit using dict

---
+++ Single_Number_II.py
@@ -0,0 +1,24 @@
+# Input: nums = [2,2,3,2]
+# Output: 3
+# Example 2:
+
+# Input: nums = [0,1,0,1,0,1,99]
+# Output: 99
+
+
+
+
+from collections import Counter
+
+nums = [2,1,2]
+count_dict = Counter(nums)
+print(count_dict)
+
+for key,value in count_dict.items():
+    if value == 1:
+        print(key)
+
+
+
+
+
