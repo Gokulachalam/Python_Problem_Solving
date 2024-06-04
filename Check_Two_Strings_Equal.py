c3cc2970e529fa39fdc0727ba64e9aaec94539a3
Gokulachalam <gokul2003ai@gmail.com>
2024-06-04 05:47:48 +0530

    Commit On 4/6 - Morning

---
+++ Check_Two_Strings_Equal.py
@@ -0,0 +1,43 @@
+# Example 1:
+
+# Input: s = "abc", t = "ahbgdc"
+# Output: true
+# Example 2:
+
+# Input: s = "axc", t = "ahbgdc"
+# Output: false
+
+# s = "g"
+# t = "ahbgdc"
+# from collections import Counter
+# a=''.join(s)
+# # c = Counter(a)
+# b = ''.join(t)
+# # d = Counter(t)
+# # print(c)
+# # print(d)
+
+# a = s.isupper()
+
+# # for i in range(len(a)):
+# #     for j in range(len(n)):
+# #         if a[i] == b[t]:
+
+# print(a)
+
+
+
+word1 = ["a", "cb"]
+word2 = ["ab", "c"]
+ 
+
+w1 = ''.join(word1)
+w2 = ''.join(word2)
+
+if w1 == w2:
+    print("true")
+else:
+    print("false")
+print(w1)
+print(w2)
+
