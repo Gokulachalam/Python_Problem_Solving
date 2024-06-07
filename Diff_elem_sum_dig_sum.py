7704e5836dfc10058c7de52de3307407485eaadd
Gokulachalam <gokul2003ai@gmail.com>
2024-06-07 09:41:28 +0530

    Commit on 7/6 - Morning

---
+++ Diff_elem_sum_dig_sum.py
@@ -0,0 +1,37 @@
+# Example 1:
+
+# Input: nums = [1,15,6,3]
+# Output: 9
+# Explanation: 
+# The element sum of nums is 1 + 15 + 6 + 3 = 25.
+# The digit sum of nums is 1 + 1 + 5 + 6 + 3 = 16.
+# The absolute difference between the element sum and digit sum is |25 - 16| = 9.
+# Example 2:
+
+# Input: nums = [1,2,3,4]
+# Output: 0
+# Explanation:
+# The element sum of nums is 1 + 2 + 3 + 4 = 10.
+# The digit sum of nums is 1 + 2 + 3 + 4 = 10.
+# The absolute difference between the element sum and digit sum is |10 - 10| = 0.
+
+
+nums = [2,7,8,10,8,10,1,10,5,9]
+first = max(nums)
+print(first)
+
+
+conv = list(map(str,nums))
+c = ''.join(conv)
+
+
+
+
+s = str(c)
+
+final = list(map(int,s))
+second = max(final)
+print(second)
+
+
+print(abs(second-first))
