2601b0236242bb8b7b8687880dee7f35fb3ddd03
Gokulachalam <gokul2003ai@gmail.com>
2024-06-11 10:10:26 +0530

    commit on 10/6 - Morning

---
+++ Max_Prod_Two_Elem.py
@@ -0,0 +1,22 @@
+# Example 1:
+
+# Input: nums = [3,4,5,2]
+# Output: 12 
+# Explanation: If you choose the indices i=1 and j=2 (indexed from 0), you will get the maximum value, that is, (nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12. 
+# Example 2:
+
+# Input: nums = [1,5,4,5]
+# Output: 16
+# Explanation: Choosing the indices i=1 and j=3 (indexed from 0), you will get the maximum value of (5-1)*(5-1) = 16.
+# Example 3:
+
+# Input: nums = [3,7]
+# Output: 12
+
+nums = [3,4,5,2]
+new = []
+for i in range(len(nums)):
+    for j in range(i+1,len(nums)):
+         ans = (nums[i]-1)*(nums[j]-1) 
+         new.append(ans)
+print(max(new))
