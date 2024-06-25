1bb8d7264ed0ebb03608ac0752272668e9d20065
Gokulachalam <gokul2003ai@gmail.com>
2024-06-25 09:48:34 +0530

    boolean updated

--- Prep/dup_words_in_str.py
+++ Prep/dup_words_in_str.py
@@ -1,9 +1,16 @@
 from collections import Counter
-input_string = "this is a test string with test words and some words repeating"
+input_string = "this is a good boy is a"
 new=[]
+is_dup=False
 words = input_string.split()
 c = Counter(words)
 for key,value in c.items():
     if value>1:
         new.append(key)
+        is_dup=True
+if is_dup:
+    print("yes dup")
+else:
+    print("no dup")
+
 print(new)
