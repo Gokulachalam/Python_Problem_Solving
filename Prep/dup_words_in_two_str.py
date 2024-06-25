cf2245827d6bd7bfc7218f245bf9fcbde37b8e61
Gokulachalam <gokul2003ai@gmail.com>
2024-06-25 10:10:44 +0530

    updated solution

--- Prep/dup_words_in_two_str.py
+++ Prep/dup_words_in_two_str.py
@@ -1,27 +1,19 @@
-
 from collections import Counter
 string1 = "apple banana mango orange"
 string2 = "grape apple orange pineapple"
-is_dup =- False
+is_dup = False
 new=[]
 s1 = string1.split()
 s2 = string2.split()
-c1 = Counter(s1)
-c2 = Counter(s2) 
-v1=[]
-v2=[]
-for key1,value1 in c1.items():
-    v1.append(key1)
-for key2,value2 in c2.items():
-    v2.append(key2)
 
-for i in range(len(v1)):
-    for j in range(len(v2)):
-        if v1[i] == v2[j]:
+for i in range(len(s1)):
+    for j in range(len(s2)):
+        if s1[i] == s2[j]:
+            new.append(s1[i])
             is_dup=True
-            new.append(v1[i])
 if is_dup:
-        print("yes duplicates")
+        print("yes dup")
 else:
-        print("no duplicates")
+        print("no dup")
+           
 print(new)
